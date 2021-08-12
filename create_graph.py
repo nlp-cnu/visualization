7aeb93598a2daa215ad34a355fb6971f469ea321
Rafael Diaz <rafael.diaz5512@gmail.com>
2021-08-12 02:53:52 -0400

    Fixed text wrapping issue, node font size maxes at size 32767.

--- create_graph.py
+++ create_graph.py
@@ -197,7 +197,7 @@ def graph_to_cytoscape(project_name):
     edges = pd.read_csv(project_name + '_edges.csv')
 
     nodes['id'] = nodes.id.astype(str)
-    nodes['font'] = nodes['size'].map(lambda x: x / 3)  # adding font sizes -> scale factor here
+    nodes['font'] = nodes['size'].map(lambda x: 32767 if x / 3 > 32767 else x / 3)  # font sizes -> scale factor here
     nodes.at[len(nodes['id'].tolist()) - 1, 'name'] = 'root'  # change name of root for clearness
 
     # must be strings for cytoscape to read them in
@@ -205,10 +205,11 @@ def graph_to_cytoscape(project_name):
     edges['target'] = edges['target'].astype(str)
 
     p4c.create_network_from_data_frames(nodes, edges, title="lbd_test", collection="tests")
+    p4c.toggle_graphics_details()
     p4c.set_node_shape_default('ELLIPSE')  # default shape of ALL nodes - except root
     p4c.set_node_color_default('#00FF00')  # color should NOT appear
     p4c.set_node_size_mapping('id', nodes['id'].tolist(), nodes['size'].tolist(), mapping_type='d')
-    p4c.set_node_font_size_mapping('name', nodes['name'].tolist(), nodes['font'].tolist(), mapping_type='d')
+    p4c.set_node_font_size_mapping('id', nodes['id'].tolist(), nodes['font'].tolist(), mapping_type='d')
     p4c.set_edge_line_width_bypass(edges['name'].tolist(), edges['width'].tolist())
     p4c.set_edge_color_bypass(edges['name'].tolist(), edges['color'].tolist())
     p4c.set_node_shape_bypass(['root'], 'HEXAGON')  # root becomes hexagon
